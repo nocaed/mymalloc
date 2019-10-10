dde407b106362af412dc34a4777a662083d08d80
saquarah <ses00019@gmail.com>
2019-10-10 15:49:49 -0400

    added include statements

--- mymalloc.h
+++ mymalloc.h
@@ -1,6 +1,8 @@
 #ifndef _HEADER_H
 #define malloc(x) mymalloc(x, __FILE__, __LINE__)
 #define free(x) myfree(x, __FILE__, __LINE__)
+#include <stdlib.h>
+#include <stdio.h>
 #endif
 
 void* mymalloc(int size);
