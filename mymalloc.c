383f00d1c10d99200d10f2e4e0fb06fb09016f7e
saquarah <ses00019@gmail.com>
2019-10-11 14:14:11 -0400

    Started mymalloc, commented out problematic definitions

---
+++ mymalloc.c
@@ -0,0 +1,10 @@
+#include "mymalloc.h"
+
+// This main method is just for testing purposes, the final product should not have it
+int main(int argc, char** argv) {
+    
+}
+
+void* mymalloc(int size) {
+
+}
--- mymalloc.h
+++ mymalloc.h
@@ -1,6 +1,6 @@
 #ifndef _HEADER_H
-#define malloc(x) mymalloc(x, __FILE__, __LINE__)
-#define free(x) myfree(x, __FILE__, __LINE__)
+//#define malloc(x) mymalloc(x, __FILE__, __LINE__)
+//#define free(x) myfree(x, __FILE__, __LINE__)
 #include <stdlib.h>
 #include <stdio.h>
 #endif
