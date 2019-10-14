3399ee9a33ad80b6daba087aab72c7fd31c6a387
Thomas C. Brewer <tcb93@ice.cs.rutgers.edu>
2019-10-14 11:40:08 -0400

    Added test case D

--- memgrind.c
+++ memgrind.c
@@ -81,7 +81,7 @@ void secondTest()
 }
 
 // randomly choose between malloc and free
-void thirdCase()
+void thirdTest()
 {
     // seed the random number generator
     srand((unsigned) time(0));
@@ -147,4 +147,98 @@ int genRandomIntByRange(int lower, int upper)
     int randNum = rand() % (upper - lower + 1) + lower;
     
     return randNum;
+}
+
+// same as C, but with variable byte sizes to allocate
+void fourthTest()
+{
+    // seed the random number generator
+    srand((unsigned) time(0));
+
+    const int MEMORY_CAPACITY = 4096; // holds the maximum memory in bytes
+
+    const short metadataSize = sizeof(metadata); // holds the size of metadata
+
+    int remainingMemory = MEMORY_CAPACITY; // initialize remaining memory to total memory
+    int mallocCntr = 0; // keeps track of the amount of calls to malloc
+    int ptrsToFree = 0; // keeps track of how many pointers need to be freed
+    int randNum; // generates a random number to determine a malloc (0) or free(1) call
+    int i; // iterates through the array of pointers to free them all
+    int mallocSize; // the number of bytes currently being malloc'd
+    int ttlMallocBytes; // mallocSize + the size of metadata
+
+    bool fiftyMallocs = false; // loop controller
+
+    void* ptr; // current pointer to allocated memory
+
+    void* ptrArr[fifty]; // array of pointers to allocated memory
+    
+    int mallocSizes[fifty]; // array of sizes of allocated memory blocks
+
+    // while malloc has not been called fifty times
+    while(fiftyMallocs == false)
+    {
+        // choose between malloc or free
+        randNum = genRandomIntByRange(0, 1);
+
+        // malloc roll
+        if(randNum == 0)
+        {
+            // roll a number between [1, 64] in bytes to allocate
+            mallocSize = genRandomIntByRange(1, 64);
+            // total amount of bytes in memory will be allocated bytes plus the size of their metadata
+            ttlMallocBytes = mallocSize + metadataSize;
+
+            // if there is not enough memory...
+            if(ttlMallocBytes > remainingMemory)
+            {
+                // ...print an error message
+                printf("Error, cannot allocate more than %d bytes.\n", remainingMemory + metadataSize);
+            }
+            // otherwise, proceed with malloc
+            else
+            {
+                // create a pointer to the allocated memory
+                ptr = malloc(mallocSize);
+                // add the pointer to the array
+                ptrArr[ptrsToFree] = ptr;
+                // add the amount of bytes now taken up by the allocated memory + metadata to mallocSizes
+                mallocSizes[ptrsToFree] = ttlMallocBytes;
+                // subtract the amount of bytes now in use from the remaining memory
+                remainingMemory -= mallocSizes[ptrsToFree];
+
+                // increment the counter of malloc calls
+                mallocCntr++;
+                // increment the amount of pointers that need to be freed
+                ptrsToFree++;
+            }
+        }
+        // otherwise, free roll
+        else
+        {
+            // if there are pointers to free...
+            if(ptrsToFree != 0)
+            {
+                // ...free the most recent pointer!
+                free(ptrArr[ptrsToFree-1]);
+                // add the freed bytes back to the remaining memory
+                remainingMemory += mallocSizes[ptrsToFree-1];
+                // decrement the amount of pointers to free
+                ptrsToFree--;
+            }
+        }
+
+        // if there have been fifty calls of malloc...
+        if(mallocCntr == fifty)
+        {
+            // ...set loop condition to end
+            fiftyMallocs = true;
+        }
+    }
+
+    // free all remaining allocated memory
+    for(i = 0; i < ptrsToFree; i++)
+    {
+        free(ptrArr[i]);
+    }
 }
